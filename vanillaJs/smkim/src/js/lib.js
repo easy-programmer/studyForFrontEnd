078a678c5d89a793370b936a0677f80789fad906
tjdah13 <tjdah0516@naver.com>
2019-03-07 19:49:03 +0900

    commit

---
+++ vanillaJs/smkim/src/js/lib.js
@@ -0,0 +1,11 @@
+export const pi = Math.PI;
+
+export function power(x, y) {
+  return x ** y;
+}
+
+export class Person {
+  constructor(name) {
+    this.name = name;
+  }
+}
---
+++ vanillaJs/smkim/webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  entry: ['@babel/polyfill', './src/js/entry.js', './src/sass/main.scss'],
+  // 컴파일 + 번들링된 js 파일이 저장될 경로와 이름 지정
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'js/bundle.js'
+  },
+  plugins: [
+    // 컴파일 + 번들링 CSS 파일이 저장될 경로와 이름 지정
+    new MiniCssExtractPlugin({ filename: 'css/style.css' })
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        include: [
+          path.resolve(__dirname, 'src/js')
+        ],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        },
+        exclude: /node_modules/
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader?outputStyle=expanded'
+          // 'sass-loader?outputStyle=compressed'
+        ],
+        exclude: /node_modules/
+      }
+    ]
+  },
+  devtool: 'source-map',
+  // https://webpack.js.org/concepts/mode/#mode-development
+  mode: 'development'
+};
