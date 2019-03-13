293a41c679b2297d09010fb03abd275ef172db3e
task-jslim <nagaeatsuh@naver.com>
2019-03-13 21:43:19 +0900

    리스트 추가

--- vanillaJs/jslim/index.js
+++ vanillaJs/jslim/index.js
@@ -1,9 +1,11 @@
 const current = document.querySelector('#current');
 const firstImg = document.querySelector('#first-img');
-const imgList = document.querySelector('#img-list');
+const imgList = document.querySelector('#img-div');
+const textList = document.querySelector('#text-div');
 const gallery = document.querySelector('#gallery');
 const list = document.querySelector('#list');
 const imgs = document.querySelectorAll('.imgs img');
+const texts = document.querySelectorAll('#text-list li');
 const opacity = 0.4;
 
 // Set first img opacity
@@ -12,6 +14,8 @@ imgs[0].style.opacity.opacity;
 //imgs.forEach(img => img.addEventListener('click', e => (current.src = e.target.src)));
 imgs.forEach(img => img.addEventListener('click', imgClick));
 
+texts.forEach(li => li.addEventListener('click', textClick));
+
 //button addEvent List
 gallery.addEventListener('click',clickGallery);
 list.addEventListener('click',clickList);
@@ -19,7 +23,7 @@ list.addEventListener('click',clickList);
 function imgClick(e) {
     // Reset th opacity
     imgs.forEach(img => (img.style.opacity = 1));
-    // Change Current image to src of clicked imgage
+    // Change Current image to src of clicked image
     current.src = e.target.src;
 
     // Add fade in class
@@ -32,16 +36,30 @@ function imgClick(e) {
     e.target.style.opacity = opacity;
 }
 
+function textClick(e) {
+    
+    // Change Current image to src of clicked imgage
+    current.src = e.target.nextElementSibling.value;
 
+    // Add fade in class
+    current.classList.add('fade-in');
+
+    // Remove fade-in class after .5 seconds
+    setTimeout(() => current.classList.remove('fade-in',500));
+
+    // Change the opacity to opacity var
+    //e.target.style.opacity = opacity;
+}
 
 function clickGallery(e) {
-
-    firstImg.style.display = "none";
-    imgList.style.display =  "none";
+    
+    imgList.style.display =  "";
+    textList.style.display =  "none";
 
 }
 
 function clickList(e) {
-    firstImg.style.display = "";
-    imgList.style.display =  "";
+    
+    imgList.style.display =  "none";
+    textList.style.display =  "";
 }
