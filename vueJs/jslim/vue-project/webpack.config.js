0a5e75b6773efac3ea6310a9edfccfc52fb1e2f5
task-jslim <nagaeatsuh@naver.com>
2019-03-25 22:26:50 +0900

    git setup

--- vueJs/jslim/vue-project/webpack.config.js
+++ vueJs/jslim/vue-project/webpack.config.js
@@ -1,13 +1,54 @@
-const path = require('path');
-
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-  entry: ['./src/index.js'],
+  entry: './src/main.js',
   output: {
-      path: path.resolve(__dirname, 'dist/js'),
-      filename: 'bundle.js'
+    path: path.resolve(__dirname, './dist'),
+    filename: 'build.js'
+  },
+  
+  plugins: [
+    new VueLoaderPlugin(),
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({ template: './src/index.html' }),
+    
+],
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ],
+      },      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+        options: {
+          loaders: {
+          }
+          // other vue-loader options go here
+        }
+      },
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(png|jpg|gif|svg)$/,
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]?[hash]'
+        }
+      }
+    ]
   },
   devServer: {
-    contentBase: './dist'
+    open: true,
+    hot: true
   }
-};
+}
